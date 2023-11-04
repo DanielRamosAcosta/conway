5734443e9fd331c737330f90fe39dad017fd3a94
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 12:58:57 +0100

    green: canCompareTwoDifferentBoards

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,15 +1,34 @@
 package org.example;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Board {
 
+  private final List<List<Cell>> cells;
+
+  public Board(final List<List<Cell>> cells) {
+    this.cells = cells;
+  }
+
   public static Board create(final List<List<Cell>> cells) {
-    return new Board();
+    return new Board(cells);
   }
 
   @Override
-  public boolean equals(final Object obj) {
-    return true;
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final Board board = (Board) o;
+    return Objects.equals(cells, board.cells);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(cells);
   }
 }
--- src/main/java/org/example/Cell.java
+++ src/main/java/org/example/Cell.java
@@ -1,8 +1,37 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Cell {
 
+  private final boolean alive;
+
+  public Cell(final boolean alive) {
+    this.alive = alive;
+  }
+
   public static Cell dead() {
-    return new Cell();
+    return new Cell(false);
+  }
+
+  public static Cell alive() {
+    return new Cell(true);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final Cell cell = (Cell) o;
+    return alive == cell.alive;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(alive);
   }
 }
