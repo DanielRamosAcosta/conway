6433cc4234090b3ac26589a99b51bbb31b57daed
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 10:34:23 +0100

    refactor: change to string literal

--- src/test/java/org/example/GameTest.java
+++ src/test/java/org/example/GameTest.java
@@ -8,17 +8,17 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
   @Test
   void an_empty_games_remain_the_same() {
-    Game game = Game.from(
-        "---\n" +
-        "---\n" +
-        "---");
+    Game game = Game.from("""
+            ---
+            ---
+            ---""");
 
     game.nextGeneration();
 
     final String string = game.toString();
-    assertThat(string).isEqualTo(
-        "---\n" +
-        "---\n" +
-        "---");
+    assertThat(string).isEqualTo("""
+      ---
+      ---
+      ---""");
   }
 }
